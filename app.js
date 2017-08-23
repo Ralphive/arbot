294858845553cd32aea4cc936245c1665cf52fc8
Ralph <Ralph@Ralphs-MacBook-Pro.local>
2017-08-23 16:26:28 +0100

    Start Kraken price comparison

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 //** App to help doing BTC Arbitrage in between Markets */
 //Load External Dependencies
-var LBCClient = require('localbitcoins-api');
-var request = require ('request');
+const LBCClient = require('localbitcoins-api');
+const request = require ('request');
+//const KrakenClient = require('kraken-api');
 
 //Initialize Parameters
-var lbc = new LBCClient("","");
+const lbc   = new LBCClient("","");
+//const kraken= new KrakenClient("", "");
+
 var ad_id; //set to value when applicable 
 var params = {}; 
   
-//General Config
-var config = {
-    LBCuser : 'starnickel/',    // Your username on LocalBitcoin,
-    EXsrc:  'http://api.coindesk.com/v1/bpi/currentprice/', // Exchange Rate source
-    sellFee: 0.01,           // 1% fees on Local Btc Selling
-    maxSellAmount: 1900 // Helps to filter high volume sellers
+//General configLBC
+var configLBC = {
+    user : 'starnickel/',   
+    sellFee: 0.01,          // %
+    maxSellAmount: 1900     // Helps to filter high volume sellers
 };
 
 var buyFrom = {
         CountryCode: 'GB/',
         CountryName: "United_Kingdom/",
-        Currency: 'GBP',       //Used in EX rate 
+        Currency: ['GBP','EUR'] ,     //LocalBTC, Kraken
         Payment1: "national-bank-transfer/",
         Payment2: null,          // To be implemented
         CurrPrice: null
@@ -38,16 +40,12 @@ var sellTo = {
 
 
 //Get Current User Feedback Score
-var UserScore = getUserScore(config.LBCuser)
-
-console.log('Retrieving Exchange Rates from '+ config.EXsrc)
-//Get BTC Data
-//buyFrom.CurrPrice = getExRate(buyFrom.Currency);
-//sellTo.CurrPrice  = getExRate(sellTo.Currency);
+//var UserScore = getUserScore(configLBC.user)
 sellTo.FoxBitOrders = getLastOrderFoxBit(sellTo.Currency);
 
 //Get Fiat Data
-var FiatExRate = getfiatExRate();
+var FiatExRate = {};
+getfiatExRate();
 
 var buyOffers = null;
 var sellOffers = null;
@@ -73,7 +71,6 @@ retrieveOffersLocalBTC(buyFrom, 'Payment1', function (data){
     });
 });
 
-
 function retrieveOffersLocalBTC(ads, payment, callback){
     lbc.api('buy-bitcoins-online', formatUrl(ads,payment), params, function(error, data) {
         if(error) {
@@ -84,7 +81,6 @@ function retrieveOffersLocalBTC(ads, payment, callback){
         }
     });
 }
-
 
 function makeAnalysis(buy, sell, buy2, sell2)
 {   
@@ -99,7 +95,7 @@ function makeAnalysis(buy, sell, buy2, sell2)
         cheapSell = findCheapestAd(sell2,cheapSell,true);
    
     //Fees and Values
-    var SellingFee = cheapSell.data.temp_price_xe * config.sellFee
+    var SellingFee = cheapSell.data.temp_price_xe * configLBC.sellFee
     var arbRate =((cheapSell.data.temp_price-SellingFee)/cheapBuy.data.temp_price_xe -1)*100;
 
     console.log('>>>>>>>>>>> LOCAL BITCOINS <<<<<<<<<<<')    
@@ -111,7 +107,7 @@ function makeAnalysis(buy, sell, buy2, sell2)
                 ' i.e. - '+ buyFrom.Currency+' '+rnd(cheapSell.data.temp_price_xe))
 
     console.log('Current NET Arbitrage rate is '+rnd(arbRate)+'%')
-    //console.log('Deducted fee '+SellingFee+'('+config.sellFee*100+'%)' )
+    //console.log('Deducted fee '+SellingFee+'('+configLBC.sellFee*100+'%)' )
     console.log('Buy Ad - '+cheapBuy.actions.public_view)
     console.log('Sell Ad - '+cheapSell.actions.public_view)
     process.exit();        
@@ -134,22 +130,6 @@ function getUserScore(userName){
     });
 }
 
-function getExRate(currency){
-    
-    request(config.EXsrc+currency+'/.json', function (error, response, body) {
-        if (error){
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-            console.log('error:', error); // Print the error if one occurred 
-        }else{
-            body = JSON.parse(body);
-            console.log(currency + ' Exchange rate on ' + body.time.updated + ' is '+ body.bpi[currency].rate_float )
-            return body.bpi[currency].rate_float;
-
-        }        
-        console.log('body:', body); // Print the HTML for the Google homepage. 
-      });
-}
-
 function getLastOrderFoxBit(currency){
     
     var url = 'https://api.blinktrade.com/api/v1/'+currency+'/ticker';
@@ -170,9 +150,15 @@ function getLastOrderFoxBit(currency){
       });
 }
 
-function getfiatExRate(currency){
+function getfiatExRate(){
     
-    var url = 'https://api.fixer.io/latest?base='+buyFrom.Currency+'&symbols='+sellTo.Currency
+    var symbols=buyFrom.Currency[0]
+    
+    for (var i = 1 ;i < buyFrom.Currency.length;i++){
+        symbols+=','+buyFrom.Currency[i];
+    }
+    
+    var url = 'https://api.fixer.io/latest?base='+sellTo.Currency+'&symbols='+symbols;
     
     request(url, function (error, response, body) {
         if (error){
@@ -180,9 +166,12 @@ function getfiatExRate(currency){
             console.log('error:', error); // Print the error if one occurred 
         }else{
             body = JSON.parse(body);
-            console.log(buyFrom.Currency +' x '+sellTo.Currency+' exchange rate is '+body.rates[sellTo.Currency]);
-            FiatExRate = body.rates[sellTo.Currency]
-            return (body.rates[sellTo.Currency]);
+            
+            for (var key in body.rates) {
+                FiatExRate[key] = 1/body.rates[key]             
+                
+                console.log(key +' x '+sellTo.Currency+' exchange rate is '+FiatExRate[key]);
+             }
         }        
       });
 }
@@ -209,9 +198,9 @@ function findCheapestAd(adlist, prevCheap, isSelling){
         cheapestOffer  = adlist[i]
         
         if(isSelling)
-            cheapestOffer.data.temp_price_xe = cheapestOffer.data.temp_price / FiatExRate;
+            cheapestOffer.data.temp_price_xe = cheapestOffer.data.temp_price / FiatExRate['GBP'];
         else
-            cheapestOffer.data.temp_price_xe = cheapestOffer.data.temp_price * FiatExRate;
+            cheapestOffer.data.temp_price_xe = cheapestOffer.data.temp_price * FiatExRate[cheapestOffer.data.currency];
         
         return cheapestOffer;
      }  
@@ -228,7 +217,7 @@ function validBuyingAd(ad){
 }
 
 function validSellingAd(ad){
-    if (ad.data.min_amount > config.maxSellAmount)
+    if (ad.data.min_amount > configLBC.maxSellAmount)
         return false;
     return true;
 
