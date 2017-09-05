15c46eb04474ccf5be647e8ac2bb9221fbea312e
Ralph <Ralph@Ralphs-MacBook-Pro.local>
2017-09-05 12:19:31 +0100

    Public API output

--- app.js
+++ app.js
@@ -26,60 +26,69 @@ var sellTo = {
         Payment: ['national-bank-transfer/','transfers-with-specific-bank/'],
     };
 
-//Get exchange rates    
-Fiat.getExRate(sellTo.Currency,buyFrom.Currency, function(rates)
-{
-    FiatExRate = rates
-})
-
-
 var buyok = false;
 var selok = false;
 var foxok = false;
 var kraok = false;
 
-//Get Selling price on Foxbit
-var Url = 'https://api.blinktrade.com/api/v1/'+sellTo.Currency+'/ticker';
-getPriceExchange(Url,function(body){
-    var fee = 0.0189; //0,5% Taker + 1.39% Withdraw    
-    console.log(sellTo.Currency +  ' Foxbit - High:'+body.high+
-                            ' - Low: '+body.low+
-                            ' - Last: '+body.last);
-    SelOptions.push(
-        formatOption('FoxBit', rnd(netPrice(body.last,-fee)), sellTo.Currency, 'https://exchange.foxbit.com.br'));
-});
 
-//Get Buying price on Kraken
-Url = 'https://api.kraken.com/0/public/Ticker?pair=BTC'+buyFrom.Currency[1]
-getPriceExchange(Url, function(body){
-    var fee = 0.0026; //0,26% Taker Order
-    body = body.result.XXBTZEUR;    
-    console.log(buyFrom.Currency[1] +  ' Kraken - High:'+body.h[0]+
-                            ' - Low: '+body.l[0]+
-                             ' - Last: '+body.c[0])
-    buyOptions.push(
-        formatOption('Kraken', rnd(netPrice(body.c[0],fee)), buyFrom.Currency[1], 'https://www.kraken.com/u/trade'));
+
+run(function(data){
+    console.log(JSON.stringify(data));
+    process.exit();    
 })
 
-// Get Buying Options from LocalBtc
-retrieveOffers(buyFrom,function(ad){
-    //no Fee for buying
-    buyOptions.push(
-        formatOption('LocalBTC',rnd(ad.data.temp_price),
-            ad.data.currency, ad.actions.public_view))
-    buyok = true
-    makeAnalysisIfDone()
-})
+function run(callback){
 
-// Get Selling Options from LocalBtc
-retrieveOffers(sellTo,function(ad){
-    var fee = 0.01; //0,1% Sale
-    SelOptions.push(
-        formatOption('LocalBTC',rnd(netPrice(ad.data.temp_price,-fee)),
-            ad.data.currency, ad.actions.public_view))
-    selok = true
-    makeAnalysisIfDone()
-})
+    //Get exchange rates    
+    Fiat.getExRate(sellTo.Currency,buyFrom.Currency, function(rates)
+    {
+        FiatExRate = rates
+    })
+
+    //Get Selling price on Foxbit
+    var Url = 'https://api.blinktrade.com/api/v1/'+sellTo.Currency+'/ticker';
+    getPriceExchange(Url,function(body){
+        var fee = 0.0189; //0,5% Taker + 1.39% Withdraw    
+        console.log(sellTo.Currency +  ' Foxbit - High:'+body.high+
+                                ' - Low: '+body.low+
+                                ' - Last: '+body.last);
+        SelOptions.push(
+            formatOption('FoxBit', rnd(netPrice(body.last,-fee)), sellTo.Currency, 'https://exchange.foxbit.com.br'));
+    });
+
+    //Get Buying price on Kraken
+    Url = 'https://api.kraken.com/0/public/Ticker?pair=BTC'+buyFrom.Currency[1]
+    getPriceExchange(Url, function(body){
+        var fee = 0.0026; //0,26% Taker Order
+        body = body.result.XXBTZEUR;    
+        console.log(buyFrom.Currency[1] +  ' Kraken - High:'+body.h[0]+
+                                ' - Low: '+body.l[0]+
+                                ' - Last: '+body.c[0])
+        buyOptions.push(
+            formatOption('Kraken', rnd(netPrice(body.c[0],fee)), buyFrom.Currency[1], 'https://www.kraken.com/u/trade'));
+    })
+
+    // Get Buying Options from LocalBtc
+    retrieveOffers(buyFrom,function(ad){
+        //no Fee for buying
+        buyOptions.push(
+            formatOption('LocalBTC',rnd(ad.data.temp_price),
+                ad.data.currency, ad.actions.public_view))
+        buyok = true
+        makeAnalysisIfDone(callback)
+    })
+
+    // Get Selling Options from LocalBtc
+    retrieveOffers(sellTo,function(ad){
+        var fee = 0.01; //0,1% Sale
+        SelOptions.push(
+            formatOption('LocalBTC',rnd(netPrice(ad.data.temp_price,-fee)),
+                ad.data.currency, ad.actions.public_view))
+        selok = true
+        makeAnalysisIfDone(callback)
+    })
+}
 
 
 function retrieveOffers(params,callback){
@@ -98,38 +107,44 @@ function retrieveOffers(params,callback){
     };
 }
 
-function makeAnalysisIfDone(){
+function makeAnalysisIfDone(callback){
     if(buyok && selok)
-        mankeAnalysis();
+        mankeAnalysis(callback);
 };
 
 
-function mankeAnalysis(){
+function mankeAnalysis(callback){
     //Calculates Arbitrage Price for each buying X selling Option
-    console.log('Sera feita analise');
-
+    var output = [buyOptions.length];
     for(var i = 0; i<buyOptions.length;i++){
         console.log('>>>>>>>>>>>>>>>>> BUY ON '+ buyOptions[i].src+ ' <<<<<<<<<<<<<<<<<')
         console.log('Price: '+buyOptions[i].currency+' '+ buyOptions[i].value)
+        output[i]=buyOptions[i];
         for(var j = 0; j<SelOptions.length;j++){
             if (j==0){
                 var buylocal = rnd(buyOptions[i].value*FiatExRate[buyOptions[i].currency])
                 console.log('Price: '+SelOptions[j].currency+' '+ buylocal)            
+                
+                output[i].localValue = buylocal;
+                output[i].localCurrency = SelOptions[j].currency;
+                output[i].exRate = rnd(FiatExRate[buyOptions[i].currency])
+                output[i].sell = [];                
             }
             console.log('>>>>>>>>>>>>>>>>> SELL ON '+ SelOptions[j].src+ ' <<<<<<<<<<<<<<<<<')   
             console.log('Price: '+SelOptions[j].currency+' '+ SelOptions[j].value)
             var sellLocal = rnd(SelOptions[j].value/FiatExRate[buyOptions[i].currency])
             console.log('Price: '+buyOptions[i].currency+' '+ sellLocal)            
-            console.log('NET ARBITRAGE RATE: '+rnd((SelOptions[j].value/buylocal-1)*100)+'%');      
+            var arbitrage = rnd((SelOptions[j].value/buylocal-1)*100);
+            console.log('NET ARBITRAGE RATE: '+ arbitrage+'%'); 
+            console.log('Link: '+ SelOptions[j].link); 
+            
+            output[i].sell[j] = SelOptions[j];
+            output[i].sell[j].arbitrage = arbitrage;
         }
         console.log('--------------------------------------------------------')   
     }
-
-    process.exit();        
-    
-
-
-
+    output
+    callback(output);
 }
 
 function getPriceExchange(url,callback){
