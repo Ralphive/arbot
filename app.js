10cd0831489bf2aef793b1cec0f9dcadb9d917d2
Ralph <ralphrpo@gmail.com>
2017-11-23 17:32:46 +0000

    Changed port for Cloud Foundry

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var path = require('path');
-var port = 8083
+var port = 8080
 
 //Configure Express 
 var app = express();
